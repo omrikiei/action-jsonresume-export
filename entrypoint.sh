b7ec840ff41c9922622c06459248de903b186670
Omri Eival <omrieival@gmail.com>
2022-05-06 11:44:20 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,6 @@ echo "Installing theme: ${THEME}"
 
 # NOTE: this needs to be installed locally, not globally
 npm install ${THEME_PACKAGE}
+PUPPETEER_PRODUCT=firefox npm install
 
 resume export --resume ${RESUME} --theme ${THEME} --format pdf /github/workspace/${OUTPUT}.pdf
